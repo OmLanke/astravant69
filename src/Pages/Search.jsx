617bc2df2e3b01ded3f1c19a743c3c6d73751f19
Kunal Chaturvedi <kunalmanu2468@gmail.com>
2024-06-09 05:56:03 +0530

    search

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -4,6 +4,10 @@ import ConBanTwo from '../Components/ConBanTwo'
 import Nsl from '../Components/Nsl'
 import SearchByAddress from '../Components/SearchByAddress.jsx'
 import ApartmentListing from '../Components/ApartmentListing.jsx'
+import { FaStar } from "react-icons/fa";
+import { FaStarHalf } from "react-icons/fa";
+
+
 
 const Contact = () => {
     return (
@@ -21,9 +25,240 @@ const Contact = () => {
                 </div>
 
             </div>
-            <SearchByAddress/>
+            <SearchByAddress />
 
-            <ApartmentListing />
+
+            <div className=" items-center justify-center flex flex-col gap-2">
+                <div className="flex flex-row p-2 gap-2">
+                    <div className='flex flex-col gap-4'>
+                        <div className="flex items-center lg:p-[200px]" style={{ backgroundImage: "url('/ast8.png')" }}>
+                            {/* <div className="p-2 border border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+                            <p className='pl-9 pr-9'>2 BHK</p>
+                        </div> */}
+                        </div>
+
+                        <div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    Elvis Heights
+                                </p>
+                                <p>
+                                    1-B.H.K , 800sq ft
+                                </p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    address
+                                </p>
+                                <p className='flex '>
+                                    <FaStarHalf />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-4'>
+                        <div className="flex items-center lg:p-[200px]" style={{ backgroundImage: "url('/ast9.png')" }}>
+                            {/* <div className="p-2 border border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+                            <p className='pl-9 pr-9'>2 BHK</p>
+                        </div> */}
+                        </div>
+
+                        <div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    Elvis Heights
+                                </p>
+                                <p>
+                                    1-B.H.K , 800sq ft
+                                </p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    address
+                                </p>
+                                <p className='flex '>
+                                    <FaStarHalf />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='flex flex-col gap-4'>
+                        <div className="flex items-center lg:p-[200px]" style={{ backgroundImage: "url('/ast10.png')" }}>
+                            {/* <div className="p-2 border border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+                            <p className='pl-9 pr-9'>2 BHK</p>
+                        </div> */}
+                        </div>
+
+                        <div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    Elvis Heights
+                                </p>
+                                <p>
+                                    1-B.H.K , 800sq ft
+                                </p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    address
+                                </p>
+                                <p className='flex '>
+                                    <FaStarHalf />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="flex flex-row pl-2 pr-2 pb-2 gap-2">
+                    <div className='flex flex-col gap-4'>
+                        <div className="flex items-center lg:p-[200px]" style={{ backgroundImage: "url('/ast11.png')" }}>
+                            {/* <div className="p-2 border border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+                            <p className='pl-9 pr-9'>2 BHK</p>
+                        </div> */}
+                        </div>
+
+                        <div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    Elvis Heights
+                                </p>
+                                <p>
+                                    1-B.H.K , 800sq ft
+                                </p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    address
+                                </p>
+                                <p className='flex '>
+                                    <FaStarHalf />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-4'>
+                        <div className="flex items-center lg:p-[200px]" style={{ backgroundImage: "url('/ast12.png')" }}>
+                            {/* <div className="p-2 border border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+                            <p className='pl-9 pr-9'>2 BHK</p>
+                        </div> */}
+                        </div>
+
+                        <div >
+                            <div className='flex justify-between'>
+                                <p>
+                                    Elvis Heights
+                                </p>
+                                <p>
+                                    1-B.H.K , 800sq ft
+                                </p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    address
+                                </p>
+                                <p className='flex '>
+                                    <FaStarHalf />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex flex-col gap-4'>
+                        <div className="flex items-center lg:p-[200px]" style={{ backgroundImage: "url('/ast13.png')" }}>
+                            {/* <div className="p-2 border border-2 hover:bg-white hover:text-black transition duration-500 hover:font-medium hover:cursor-pointer border-white text-white bg-transparent">
+                            <p className='pl-9 pr-9'>2 BHK</p>
+                        </div> */}
+                        </div>
+
+                        <div >
+                            <div className='flex justify-between'>
+                                <p>
+                                    Elvis Heights
+                                </p>
+                                <p>
+                                    1-B.H.K , 800sq ft
+                                </p>
+                            </div>
+                            <div className='flex justify-between'>
+                                <p>
+                                    address
+                                </p>
+                                <p className='flex '>
+                                    <FaStarHalf />
+                                    <FaStar />
+                                    <FaStar />
+                                    <FaStar />
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+            {/* ---------------------------------------- */}
+
+
+            <div className="flex flex-col justify-between bg-[#E79700] pb-96 w-full p-20">
+                <div className=" lg:text-7xl text-[#04236D]">
+                    <p>Want to find your ideal house?</p>
+                </div>
+
+                <div>
+                    <div className='text-[#04236D] text-md'>
+                        <p className='w-[40%] font-bold pt-6 tracking-widest'>
+                        Contact us and we will provide you our expert guidance and assistance in finding your dream house.
+                        </p>
+                        <div className='pt-6 font-bold'>
+                            <p>
+                            Connect with us
+                            </p>
+                            <div className='py-2'>
+                                <p className='text-white pb-4'>
+                                    <a href="info@astravant.com">
+                                        info@astravant.com
+                                    </a>
+                                </p>
+                                <p className='text-white'>
+                                    +91 8329 109 390
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+
+
+
 
 
         </>
