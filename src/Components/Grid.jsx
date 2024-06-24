e0a2b15398a9291484ee4112e94cabc3490e83a8
Om Lanke <92863779+OmLanke@users.noreply.github.com>
2024-06-24 13:30:32 +0000

    deals
    Co-authored-by: Anushrut Pandit <Anushlinux@users.noreply.github.com>

--- src/Components/Grid.jsx
+++ src/Components/Grid.jsx
@@ -5,7 +5,7 @@ import CardHome from '../Components/CardHome'
 
 export default function Example() {
   return (
-    <ul role="list" className="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
+    <ul role="list" className="overflow-x-auto grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
       {db.map((property , index) => (
         <li key={index} className="overflow-hidden rounded-xl border border-gray-200">
             <CardHome name={property.name} bhk={property.bhk} id={index} area={property.area} imgurl={property.imgurl} address={property.address} parking={property.parking} verified={property.verified} price={property.price}/>
