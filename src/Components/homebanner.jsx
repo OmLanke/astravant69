2a6641bd75abc0c5eb767189771ed643ad151525
Om Lanke <92863779+OmLanke@users.noreply.github.com>
2024-06-24 09:24:08 +0000

    Reality -> Realty

--- src/Components/homebanner.jsx
+++ src/Components/homebanner.jsx
@@ -50,7 +50,7 @@ const Banner =() => {
             </h1><br>
             </br>
             <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                Meet reality
+                Meet Realty
             </h1>
             {/* <p className="mt-6 text-lg leading-8 text-gray-300">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
