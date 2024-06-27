d929acadf29b969d21076dbafeebb7aff25b750c
Om Lanke <oplanke@gmail.com>
2024-06-27 21:03:19 +0530

    remove components

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import ConUsBanOne from '../Components/ConUsBanOne'
-import Navbar from '../Components/Navbar'
 import ConUsBanTwo from '../Components/ConUsBanTwo'
 import ConUsBanThree from '../Components/ConUsBanThree'
-import Nsl from '../Components/Nsl'
-import Footer from '../Components/Footer.jsx'
 
 const ContactUs = () => {
     return (
         <>
-            <Navbar />
             <ConUsBanOne />
             <ConUsBanTwo />
             <ConUsBanThree />
-            <Nsl />
-            <Footer />
         </>
     )
 }
