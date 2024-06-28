5cc14e798c3ce3a8870bb88ff4a2195d6ddb1a47
Anushlinux <anushrutpandit@gmail.com>
2024-06-28 22:56:40 +0530

    search changes

--- src/Components/PropListHeroSection.jsx
+++ src/Components/PropListHeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PropListHeroSection = () => {
     return (
-        <>
+        <div>
             <div className="bg-[url('/ast13.png')] text-white bg-cover flex flex-col bg-center w-full h-screen" >
                 <div className="mx-auto lg:my-auto md:my-auto sm:mt-56 text-center flex flex-col gap-8">
                     <div className="flex flex-col text-6xl font-medio">
@@ -11,7 +11,7 @@ const PropListHeroSection = () => {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
