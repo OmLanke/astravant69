69ba1dde608941975277c1851091132f90e27a7b
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-07-05 21:13:04 +0530

    Fixed ConBanTwo.jsx

--- src/Components/ConBanTwo.jsx
+++ src/Components/ConBanTwo.jsx
@@ -1,53 +1,51 @@
-import React from 'react';
-import {Helmet} from "react-helmet";
+import React from 'react'
+
 const ConBanTwo = () => {
     return (
         <div className="w-full bg-white  text-[#04236D]">
-            
-            <div className="p-8 flex flex-col">
-                <div className="flex flex-row w-full justify-between h-[100vh]">
-                    <div className="flex flex-col pl-20 pb-20 pt-20 gap-10">
-                        <div className="lg:text-5xl font-medio">
-                            <p>Astravant Realty group:</p>
-                            <p>India's #1 real estate</p>
-                            <p>agency.</p>
-                        </div>
-                        <div className="w-3/5 mt-10">
+                <div className="p-8 flex flex-col">
+                    <div className="flex flex-row w-full justify-between h-[100vh]">
+                        <div className="flex flex-col pl-20 pb-20 pt-20 gap-10">
+                            <div className="lg:text-5xl font-medio">
+                                <p>Astravant Realty group:</p>
+                                <p>India's #1 real estate</p>
+                                <p>agency.</p>
+                            </div>
+                            <div className="w-3/5 mt-10 ">
+                                <p><meta name="description" content="Astravant Realty Group: Mumbai's leading luxury real estate agency. Offering exclusive properties in Juhu, Bandra, and South Bombay with unparalleled expertise and service."/> There is nothing like it ~ anywhere. This is a once-in-a-lifetime opportunity to possess a natural oasis located in the exclusive neighborhoods of <strong>Mumbai</strong>, the ultimate setting for creating a lifestyle of custom luxury. By any measure, <strong>Astravant Realty Group</strong> is one of <em>Mumbai's</em> most successful, creative, and sought-after luxury real estate agencies.</p> <p>Not only are we the No. 1 agency in <em>South Bombay</em>, but we also lead the top-ranked team across <em>Mumbai</em>. Since our expansion, Astravant Realty Group holds the number one position in <em>Juhu, Bandra, and South Bombay</em>. Our success has led to us being recognized as the number one luxury team in <em>Mumbai</em> for 2023.
+                                </p>
+                               <p>Astravant's success has resulted in invitations to exclusive, invite-only agent networks, connecting us with international luxury real estate markets and enhancing our <strong>global reach</strong> while maintaining our deep roots in Mumbai.</p>
+                               <div className="lg:text-2xl font-medio mt-3">
+                               <p>Our Vision:</p>
+                                <p>Redefining Luxury Real Estate in Mumbai</p>
+                            </div>
+                               <p><meta name="description" content="Astravant Realty: Delivering unparalleled luxury real estate experiences in Mumbai. Exclusive service, bespoke solutions, and a commitment to excellence in every interaction."/>
+At <strong>Astravant Realty</strong>, our vision transcends traditional real estate services. We aspire to craft <em>unparalleled luxury experiences</em> for Mumbai's most discerning clientele. Our commitment goes beyond mere transactions; we offer a journey of <strong>opulence and sophistication</strong> in every interaction. From the moment you engage with us, whether for <em>acquiring a palatial South Bombay residence</em>, <em>investing in premium Bandra properties</em>, or <em>exploring exclusive Juhu listings</em>, you step into a world of refined elegance.</p>
+<p>We pledge to deliver a level of service that is as luxurious as the properties we represent. Our team of elite professionals is dedicated to providing bespoke solutions, anticipating needs before they arise, and ensuring that every aspect of your real estate journey exudes prestige. At Astravant, we don't just meet expectations – we redefine them, setting new standards in <strong>Mumbai's luxury real estate market</strong>.</p> 
+<p><meta name="keywords" content="Astravant Realty, luxury real estate Mumbai, exclusive property services, South Bombay residences, Bandra premium properties, Juhu exclusive listings, bespoke real estate solutions, luxury property experience"/> </p>
 
+                            
+                            </div>
+                        </div>
+                        <img src="bui.png" className='h-full' alt="" />
+                    </div>
+                    <div className=" flex flex-col pl-20  gap-6 pt-10">
+                        
                         <p>
-                        <Helmet>
-                            <meta name="description" content="Astravant Realty Group: Mumbai's leading luxury real estate agency. Offering exclusive properties in Juhu, Bandra, and South Bombay with unparalleled expertise and service."/> </Helmet>There is nothing like it ~ anywhere. This is a once-in-a-lifetime opportunity to possess a natural oasis located in the exclusive neighborhoods of <strong>Mumbai</strong>, the ultimate setting for creating a lifestyle of custom luxury. By any measure, <strong>Astravant Realty Group</strong> is one of <em>Mumbai's</em> most successful, creative, and sought-after luxury real estate agencies.</p> <p>Not only are we the No. 1 agency in <em>South Bombay</em>, but we also lead the top-ranked team across <em>Mumbai</em>. Since our expansion, Astravant Realty Group holds the number one position in <em>Juhu, Bandra, and South Bombay</em>. Our success has led to us being recognized as the number one luxury team in <em>Mumbai</em> for 2023.</p> <p>Astravant's success has resulted in invitations to exclusive, invite-only agent networks, connecting us with international luxury real estate markets and enhancing our <strong>global reach</strong> while maintaining our deep roots in Mumbai.</p> <p>
-                            <Helmet><meta name="keywords" content="Astravant Realty Group, Mumbai luxury real estate, South Bombay properties, Juhu real estate, Bandra luxury homes, Mumbai's top real estate agency, exclusive properties Mumbai"/> </Helmet></p>
-                        
-                        <h3></h3> 
-                        <div className="lg:text-2xl mt-5 font-medio">
-                            <p>Global Expertise, Local Mastery</p>
-                        </div>
-                        <p><meta name="description" content="Astravant Realty: Combining global real estate expertise with deep local knowledge of Mumbai's luxury property market. Offering personalized, world-class service to discerning clients."/>
-                    At <strong>Astravant Realty</strong>, we pride ourselves on our unique blend of <em>global expertise and local mastery</em>. Our team of seasoned professionals brings together decades of experience in <strong>international luxury real estate</strong> with an intimate understanding of <em>Mumbai's premium property landscape</em>. This powerful combination allows us to offer our clients unparalleled insights and opportunities in the world of high-end real estate.</p>
-                    <div className='mt-2'>
-                    <p>Whether you're a <em>local luminary</em> seeking the perfect pied-à-terre in <strong>Malabar Hill</strong>, an <em>international investor</em> eyeing lucrative opportunities in <strong>Worli's skyline</strong>, or a <em>global citizen</em> searching for a <strong>beachfront haven in Juhu</strong>, our team's diverse expertise ensures that we can meet and exceed your unique needs. We leverage our extensive network of <em>global contacts</em> and our deep roots in Mumbai to provide you with exclusive access to off-market properties and insider knowledge that can make all the difference in your real estate decisions.</p> <p><meta name="keywords" content="Astravant Realty, Mumbai luxury real estate experts, global property network, Malabar Hill properties, Worli real estate investment, Juhu beachfront homes, international real estate expertise, exclusive Mumbai properties"/> </p></div>
-                    </div>
+                        <div className="lg:text-2xl font-medio mt-3 mb-2">
+                               <p>Our Vision:</p>
+                                <p>Redefining Luxury Real Estate in Mumbai</p>
+                            </div>
+                        </p>
 
-                        
+                        <p><meta name="description" content="Astravant Realty: Combining global real estate expertise with deep local knowledge of Mumbai's luxury property market. Offering personalized, world-class service to discerning clients."/>
+                            At <strong>Astravant Realty</strong>, we pride ourselves on our unique blend of <em>global expertise and local mastery</em>. Our team of seasoned professionals brings together decades of experience in <strong>international luxury real estate</strong> with an intimate understanding of <em>Mumbai's premium property landscape</em>. This powerful combination allows us to offer our clients unparalleled insights and opportunities in the world of high-end real estate.</p>
+                            <p>Whether you're a <em>local luminary</em> seeking the perfect pied-à-terre in <strong>Malabar Hill</strong>, an <em>international investor</em> eyeing lucrative opportunities in <strong>Worli's skyline</strong>, or a <em>global citizen</em> searching for a <strong>beachfront haven in Juhu</strong>, our team's diverse expertise ensures that we can meet and exceed your unique needs. We leverage our extensive network of <em>global contacts</em> and our deep roots in Mumbai to provide you with exclusive access to off-market properties and insider knowledge that can make all the difference in your real estate decisions.</p> 
+                            <p><meta name="keywords" content="Astravant Realty, Mumbai luxury real estate experts, global property network, Malabar Hill properties, Worli real estate investment, Juhu beachfront homes, international real estate expertise, exclusive Mumbai properties"/> </p>
                     </div>
-                    <img src="bui.png" className='h-full' alt="" />
-                </div>
-                <div className="flex flex-col pl-20  gap-6 pt-10">
-                <div className="lg:text-3xl font-medio">
-                            <p>Our Vision: </p>
-                            <p>Redefining Luxury Real Estate in Mumbai</p>
-                        </div>
-                        <div className="w-3/5 mt-2 ">
-                        <p><Helmet><meta name="description" content="Astravant Realty: Delivering unparalleled luxury real estate experiences in Mumbai. Exclusive service, bespoke solutions, and a commitment to excellence in every interaction."></meta></Helmet>
-                        At <strong>Astravant Realty</strong>, our vision transcends traditional real estate services. We aspire to craft <em>unparalleled luxury experiences</em> for Mumbai's most discerning clientele. Our commitment goes beyond mere transactions; we offer a journey of <strong>opulence and sophistication</strong> in every interaction. From the moment you engage with us, whether for <em>acquiring a palatial South Bombay residence</em>, <em>investing in premium Bandra properties</em>, or <em>exploring exclusive Juhu listings</em>, you step into a world of refined elegance.</p>
-                        <p>We pledge to deliver a level of service that is as luxurious as the properties we represent. Our team of elite professionals is dedicated to providing bespoke solutions, anticipating needs before they arise, and ensuring that every aspect of your real estate journey exudes prestige. At Astravant, we don't just meet expectations – we redefine them, setting new standards in <strong>Mumbai's luxury real estate market</strong>.</p> <p><Helmet><meta name="keywords" content="Astravant Realty, luxury real estate Mumbai, exclusive property services, South Bombay residences, Bandra premium properties, Juhu exclusive listings, bespoke real estate solutions, luxury property experience"/> </Helmet></p>
-                        </div>
                 </div>
             </div>
-           
-        </div>
     )
 }
 
-export default ConBanTwo;
+export default ConBanTwo
