26e4766895ed7962dca6093eec6eb0814d1a85f8
neilo <neil.carnac@somaiya.edu>
2024-07-07 01:00:06 +0530

    feat: added ui changes

--- src/Components/ConBanOne.jsx
+++ src/Components/ConBanOne.jsx
@@ -10,23 +10,18 @@ const ConBanOne = () => {
                         <p>ASTRAVANT REALTY</p>
                     </div>
                     <div className="mx-auto">
-<<<<<<< HEAD
                         <p>Phone No. 9136114754</p>
-                        <p>Email- infor@astravant.com</p>
-=======
-                        <p>Phone No. 9136114755</p>
                         <p>Email- info@astravantrealty.com</p>
->>>>>>> 4b9e1dba9fbf0b40b9301466993e9a5bc85faa3e
-                    </div>
+                    </div >
                     <div className="p-2 pl-4 pr-4 hover:bg-white transition duration-500 hover:text-black hover:cursor-pointer font-medium flex flex-col items-center justify-center text-white border border-[2px] mb-20 border-white mx-auto">
                         <Link to="/contactus">
                             <p>Contact Us</p>
                         </Link>
                     </div>
 
-                </div>
-            </div>
-        </div>
+                </div >
+            </div >
+        </div >
     )
 }
 
