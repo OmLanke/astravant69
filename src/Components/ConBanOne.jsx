2d0a2f15b6bde95127334089ecb1ad464f7a3ad7
neilo <neil.carnac@somaiya.edu>
2024-07-07 00:09:37 +0530

    feat: fixed and pushing

--- src/Components/ConBanOne.jsx
+++ src/Components/ConBanOne.jsx
@@ -6,11 +6,11 @@ const ConBanOne = () => {
         <div>
             <div className="relative text-white bg-cover flex flex-col bg-center w-full h-screen" style={{ backgroundImage: "url('/concon.png')" }}>
                 <div className="mx-auto my-auto flex flex-col gap-8">
-                    <div className="text-7xl font-medio">
+                    <div className="lg:text-7xl text-3xl font-medio">
                         <p>ASTRAVANT REALTY</p>
                     </div>
                     <div className="mx-auto">
-                        <p>Phone No. 897567546</p>
+                        <p>Phone No. 9136114754</p>
                         <p>Email- infor@astravant.com</p>
                     </div>
                     <div className="p-2 pl-4 pr-4 hover:bg-white transition duration-500 hover:text-black hover:cursor-pointer font-medium flex flex-col items-center justify-center text-white border border-[2px] mb-20 border-white mx-auto">
