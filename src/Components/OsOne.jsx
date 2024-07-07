859e4befc4181ca79e52c63511196bcde4502893
Om Lanke <oplanke@gmail.com>
2024-07-07 16:25:36 +0530

    Medio font

--- src/Components/OsOne.jsx
+++ src/Components/OsOne.jsx
@@ -7,7 +7,7 @@ const OsOne = () => {
                 className="relative bg-cover flex flex-col bg-center w-full h-screen bg-[url('/servc.png')]"
             >
                 <div className="mx-auto my-auto text-white text-center px-4">
-                    <div className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-medium">
+                    <div className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-medio">
                         <p>Our Services</p>
                     </div>
 
